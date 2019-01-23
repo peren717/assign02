d7983a70c56d2f82e134332b1d77293af8cdd41a
peren717 <peren717@gmail.com>
2019-01-23 15:37:45 -0700

    3

---
+++ assign02/LibraryBook.java
@@ -0,0 +1,10 @@
+package assign02;
+
+public class LibraryBook extends Book {
+
+	public LibraryBook(long isbn, String author, String title) {
+		super(isbn, author, title);
+		// TODO Auto-generated constructor stub
+	}
+
+}
